2be24bce79219d4e7fd2e676914bbe52a72d04e1
Nate Croteau <natekcroteau@gmail.com>
2020-12-04 21:42:30 +0000

    Automatically backed up by Learn

--- lib/grocer_part_2.rb
+++ lib/grocer_part_2.rb
@@ -1,20 +1,30 @@
 require_relative './part_1_solution.rb'
 
 def apply_coupons(cart, coupons)
-  # Consult README for inputs and outputs
-  #
+  applied_array = []
+  
+  
+  
+  
+  
   # REMEMBER: This method **should** update cart
 end
 
 def apply_clearance(cart)
-  # Consult README for inputs and outputs
-  #
+
+
+
   # REMEMBER: This method **should** update cart
 end
 
 def checkout(cart, coupons)
-  # Consult README for inputs and outputs
-  #
+
+
+
+
+
+
+
   # This method should call
   # * consolidate_cart
   # * apply_coupons
